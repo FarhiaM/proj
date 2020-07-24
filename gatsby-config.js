181df568440d8b19f9564ca640fa03192187a129
Farhia <farhia.fcb@gmail.com>
2020-07-24 21:20:57 +0200

    Add blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,28 @@
 module.exports = {
+	 siteMetadata: {
+	    title: `Farhia Muse`,
+	    author: `Farhia Muse`,
+	    description: `Farhia Muse's personal blog site.`,
+	    siteUrl: `https://farhia-proj-site.netlify.app`,
+	    social: [
+	      {
+	        name: `Twitter`,
+	        url: `https://twitter.com/Farhia_MM`,
+	      },
+	      {
+	        name: `GitHub`,
+	        url: `https://github.com/farhiam`,
+	      },
+	    ],
+	  },
 	plugins: [
-		`gatsby-plugin-mdx`
+		`gatsby-plugin-mdx`,
+		{
+			resolve: `gatsby-theme-blog`,
+			options: {
+				basePath: `/blog`,
+				mdxOtherwiseConfigured: true
+			}
+		}
 	]
 }
